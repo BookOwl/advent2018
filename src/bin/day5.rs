0e8d95080c100e3e0fcaa4302a591ff481178738
BookOwl <stanleybookowl@gmail.com>
2018-12-06 17:13:59 -0500

    Try to speed up debug mode execution for day 5. It didn't work. :(

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -2,8 +2,7 @@ extern crate regex;
 
 static POLYMER: &str = include_str!("../../inputs/day5.txt");
 
-fn react_polymer(poly: &str) -> String {
-    let pat = regex::Regex::new(r"aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz").unwrap();
+fn react_polymer(poly: &str, pat: &regex::Regex) -> String {
     let mut poly = String::from(poly);
     let mut after_reaction = pat.replace_all(&poly, "").into_owned();
     while poly != after_reaction {
@@ -14,21 +13,21 @@ fn react_polymer(poly: &str) -> String {
 }
 
 fn part1() {
-    let poly = react_polymer(POLYMER);
+    let pat = regex::Regex::new(r"aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz").unwrap();
+    let poly = react_polymer(POLYMER, &pat);
     println!("Part 1: {}", poly.len());
 }
 
 fn part2() {
+    let pat = regex::Regex::new(r"aA|bB|cC|dD|eE|fF|gG|hH|iI|jJ|kK|lL|mM|nN|oO|pP|qQ|rR|sS|tT|uU|vV|wW|xX|yY|zZ|Aa|Bb|Cc|Dd|Ee|Ff|Gg|Hh|Ii|Jj|Kk|Ll|Mm|Nn|Oo|Pp|Qq|Rr|Ss|Tt|Uu|Vv|Ww|Xx|Yy|Zz").unwrap();
     let mut best_len = POLYMER.len() + 1;
     for (unitp, unitn) in [('a', 'A'), ('b', 'B'), ('c', 'C'), ('d', 'D'), ('e', 'E'), ('f', 'F'), 
                            ('g', 'G'), ('h', 'H'), ('i', 'I'), ('j', 'J'), ('k', 'K'), ('l', 'L'), 
                            ('m', 'M'), ('n', 'N'), ('o', 'O'), ('p', 'P'), ('q', 'Q'), ('r', 'R'), 
                            ('s', 'S'), ('t', 'T'), ('u', 'U'), ('v', 'V'), ('w', 'W'), ('x', 'X'), 
                            ('y', 'Y'), ('z', 'Z')].iter() {
-        let re = regex::Regex::new(&format!("{}|{}", unitp, unitn)).unwrap();
-
-        let poly = re.replace_all(POLYMER, "").into_owned();
-        let reacted = react_polymer(&poly);
+        let poly = POLYMER.replace(*unitn, "").replace(*unitp, "");
+        let reacted = react_polymer(&poly, &pat);
         if reacted.len() < best_len {
             best_len = reacted.len();
         }
